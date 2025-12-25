38f25af58881c2944982cf245cb40ccea6e2b009
pablo <ortegaperaltap@gmail.com>
2025-12-25 17:41:30 -0400

    create migrations folder

--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0420fa72d28abeffbb77ab4991a0b7326c296fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342723e2c18f380bf9eab347d8da689043861e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
