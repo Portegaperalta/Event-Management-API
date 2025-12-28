cdd51fe6cd85e437d588f31c4e90894a70309ac7
pablo <ortegaperaltap@gmail.com>
2025-12-28 14:39:15 -0400

    Create Event model

---
+++ Models/Event.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Event_Management_API.Models;
+
+public class Event
+{
+    public int Id {get;set;}
+    [Required]
+    public required string Name {get;set;}
+    [Required]
+    public required string Description {get;set;}
+    [Required]
+    public required string Location {get;set;}
+    [Required]
+    public required DateTime StartDate {get;set;}
+    [Required]
+    public required DateTime EndDate {get;set;}
+    [Required]
+    public required TimeSpan StartTime {get;set;}
+    [Required]
+    public required TimeSpan EndTime{get;set;}
+    public string? AccessCode {get;set;}
+}
