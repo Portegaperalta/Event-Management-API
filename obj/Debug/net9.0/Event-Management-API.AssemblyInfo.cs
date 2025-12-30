6dc2f4c2cb64d0b691136d6bcd20cf42456a54b6
pablo <ortegaperaltap@gmail.com>
2025-12-30 09:52:18 -0400

    create repositories folder inside Data folder

--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82033afaab89993cca8674ed1758bd875d295ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589c99dbb2e45db83b4df17143b463fafc51b1d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
