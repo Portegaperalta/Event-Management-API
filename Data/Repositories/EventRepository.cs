ae55b392ac7b08702d07d9b6c6134250b5a1b5e1
pablo <ortegaperaltap@gmail.com>
2026-01-01 13:15:00 -0400

    Add implementations for GetAllAsync and GetByIdAsync

--- Data/Repositories/EventRepository.cs
+++ Data/Repositories/EventRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using Event_Management_API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Event_Management_API.Data.Repositories;
 
@@ -10,5 +11,15 @@ public class EventRepository : IEventRepository
     public EventRepository(ApplicationDbContext applicationDbContext)
     {
         _context = applicationDbContext;
+    }
+
+    public async Task<IEnumerable<Event>> GetAllAsync()
+    {
+        return await _context.Events.ToListAsync();
+    }
+
+    public async Task<Event?> GetByIdAsync(int eventId)
+    {
+        return await _context.Events.FirstOrDefaultAsync();
     }
 }
