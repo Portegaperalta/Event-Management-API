4d22ba1941b591ea1220b800fd9f41742c674b64
pablo <ortegaperaltap@gmail.com>
2026-01-01 12:12:51 -0400

    Add initial build artifacts and static web asset configurations for Event-Management-API

--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e96638d21df4209e69a8bb99339a56fd5d18bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3494f163e94c58251977af9ce35c2e2fea2a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
