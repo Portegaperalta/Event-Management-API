67af8ce484e073ea69f9785d0f3a0a3f55055c6d
pablo <ortegaperaltap@gmail.com>
2026-01-01 12:17:37 -0400

    move build after adding services

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Services Area 
-var app = builder.Build();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 //Middlewares Area
+var app = builder.Build();
 
 app.MapControllers();
 
