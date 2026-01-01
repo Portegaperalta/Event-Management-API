2907972f7f226c3120fc9d3c67dfc88988144432
pablo <ortegaperaltap@gmail.com>
2026-01-01 11:01:08 -0400

    install ef core design

--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069ae9f75cb21939dbc5f6c704dc9fb9949be088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8656f9bdb490c7a060e1f7231dbba2339167a4b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
