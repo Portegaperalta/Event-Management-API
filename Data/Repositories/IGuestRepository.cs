8ecdb5dae88e372203175b442af96671a334ac84
pablo <ortegaperaltap@gmail.com>
2026-01-01 14:06:09 -0400

    create interface IGuestRepository and abstract fucntions

---
+++ Data/Repositories/IGuestRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using Event_Management_API.Models;
+
+namespace Event_Management_API.Data.Repositories;
+
+public interface IGuestRepository
+{
+    Task<IEnumerable<Guest>> GetAllAsync();
+    Task<Guest?> GetByIdAsync(int guestId);
+    Task CreateAsync(Guest guestData);
+    Task UpdateAsync(Guest guestData);
+    Task DeleteAsync(int guestId);
+}
--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a760d9e24ebd250056aa2d554158fad7ea5adf6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+befe744d8b7ac632d6e248d462099087c1c905c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
