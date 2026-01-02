eadfb4c010a47b54f644f32466fd271351d2458e
pablo <ortegaperaltap@gmail.com>
2026-01-02 01:29:55 -0400

    add methods implementation

---
+++ Data/Repositories/GuestRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using Event_Management_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event_Management_API.Data.Repositories;
+
+public class GuestRepository : IGuestRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public GuestRepository (ApplicationDbContext applicationDbContext)
+    {
+        _context = applicationDbContext;
+    }
+
+    public async Task<IEnumerable<Guest>> GetAllAsync()
+    {
+        return await _context.Guests.ToListAsync();
+    }
+
+    public async Task<Guest?> GetByIdAsync(int guestId)
+    {
+        return await _context.Guests.Where(g => g.Id == guestId).FirstOrDefaultAsync();
+    }
+
+    public async Task CreateAsync(Guest guestData)
+    {
+        _context.Guests.Add(guestData);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Guest guestData)
+    {
+        _context.Guests.Update(guestData);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<int> DeleteAsync(int guestId)
+    {
+        var deletedRecords = await _context.Guests.Where(g => g.Id == guestId)
+                                                  .ExecuteDeleteAsync();
+        return deletedRecords;
+    }
+}
