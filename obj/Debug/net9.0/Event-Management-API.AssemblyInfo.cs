79987e58bdcf05b1a16bfd529858dbb999ae0b7e
pablo <ortegaperaltap@gmail.com>
2026-01-06 14:51:35 -0400

    create mappers folder

--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb052abe6eaf2ee580e67f85fb71a29ec648452")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b834ef553b5f96b7d8a0a9e4a8144d32a1f3cabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
