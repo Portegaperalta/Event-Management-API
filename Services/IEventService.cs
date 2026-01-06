2e0b7cb6ba3d326c2ffe5a3bec23b74ce1e4fda6
pablo <ortegaperaltap@gmail.com>
2026-01-06 14:42:14 -0400

    change GetByIdAsync method return type to EventDTO

--- Services/IEventService.cs
+++ Services/IEventService.cs
@@ -1,4 +1,5 @@
 using System;
+using Event_Management_API.DTOS;
 using Event_Management_API.Models;
 
 namespace Event_Management_API.Services;
@@ -6,7 +7,7 @@ namespace Event_Management_API.Services;
 public interface IEventService
 {
     Task<IEnumerable<Event>> GetAllAsync();
-    Task<Event> GetByIdAsync(int eventId);
+    Task<EventDTO?> GetByIdAsync(int eventId);
     Task CreateAsync(Event eventData);
     Task UpdateAsync(Event enventUpdateData);
     Task DeleteAsync(int eventId);
