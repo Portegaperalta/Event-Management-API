17ba12106a4ab838c4be484f98c5165e29e07ac7
pablo <ortegaperaltap@gmail.com>
2026-01-07 12:56:42 -0400

    inyect EventMapper dependency

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -1,15 +1,19 @@
 using System;
 using Event_Management_API.Data;
+using Event_Management_API.DTOS;
+using Event_Management_API.Mappers;
 using Event_Management_API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Event_Management_API.Services;
 
 public class EventService : IEventService
 {
     private readonly ApplicationDbContext _context;
-
-    public EventService(ApplicationDbContext context)
+    private readonly EventMapper _eventMapper;
+    public EventService(ApplicationDbContext context,EventMapper eventMapper)
     {
         _context = context;
+        _eventMapper = eventMapper;
     }
 }
