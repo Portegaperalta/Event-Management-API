211514ecec8932485909b254a97f03ff830d515c
pablo <ortegaperaltap@gmail.com>
2026-01-08 14:55:33 -0400

    Create DTO for Guest model update

---
+++ DTOS/GuestUpdateDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Event_Management_API.DTOS;
+
+public class GuestUpdateDTO
+{
+    public int Id {get;set;}
+    [Required(ErrorMessage = "The field {0} is required")]
+    public required string FirstName {get;set;}
+    [Required(ErrorMessage = "The field {0} is required")]
+    public required string LastName{get;set;}
+    [Required(ErrorMessage = "The field {0} is required")]
+    [Phone]
+    public required string PhoneNumber {get;set;}
+}
--- obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Event-Management-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c40509ce4514ab6dee696dc4c72cef107ca11d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4398cd4e4bef195b56e7d34957d79ee39d816e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event-Management-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
